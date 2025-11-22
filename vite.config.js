fd270a6992c15b832a7a480fb41d42e69297253e
a-vidhawan <122562955+a-vidhawan@users.noreply.github.com>
2025-11-22 00:52:32 -0800

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 // here to customise the build or dev server.
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/",
   server: {
     port: 5173,
   },
